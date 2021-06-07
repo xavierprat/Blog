f466c69d8ff3b1391bdc705029598e4443193e9b
Xavier Prat-Resina <pratr001@r.umn.edu>
2021-06-07 07:38:56 -0500

    Adding posts

---
+++ docs/posts/history4kids/histokids/info.js
@@ -0,0 +1,24 @@
+/**
+ * @Author: xavier
+ * @Date:   2020-04-24T06:50:32-05:00
+ * @Last modified by:   xavier
+ * @Last modified time: 2020-05-22T10:24:28-05:00
+ */
+var info = [
+  {date:"before 10,000BC",name:"Paleolithic",charac:["stone weapons","nomads","1st fire"]},
+  {date:"10,000BC - 5,000BC",name:"Neolithic",charac:["stone weapons","1st village","1st agriculture","1st farm animals","1st wheel"]},
+  {date:"5,000BC - 1,000BC",name:"Metals age",charac:["metal tools","1st cities","1st writing","skilled jobs","sailing and trade"]},
+  {date:"3,000BC - 500BC",name:"Ancient Egypt",charac:["pyramid","papyrus"]},
+  {date:"1000BC - 100BC",name:"Ancient Greece",charac:["philosophy","democracy","architecture"]},
+  {date:"200BC - 500",name:"Roman Empire",charac:["legion","latin"]},
+  {date:"250 - 900",name:"Mayan Empire",charac:["1st American empire"]},
+  {date:"610 - 900",name:"Islamic expansion",charac:[""]},
+  {date:"800 - 1100",name:"Viking expansion",charac:[""]},
+  {date:"1095 - 1400",name:"Crusades",charac:[""]},
+  {date:"1440",name:"Printing press",charac:[""]},
+  {date:"1492",name:"Discovery of Americas",charac:[""]},
+  {date:"1600 - 1800",name:"Atlantic slave trade",charac:[""]},
+  {date:"1775-1783",name:"American Revolution",charac:[""]},
+  {date:"1789",name:"French Revolution",charac:[""]},
+  {date:"1865",name:"American Civil War",charac:[""]}
+];
