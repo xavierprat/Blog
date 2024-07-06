e468336e064672e510f8a102374ffa9761dcfadb
Xavier Prat-Resina <pratr001@r.umn.edu>
2024-07-06 14:28:36 -0500

    06Jul24

---
+++ docs/posts/history4kids/histokids/catala/colors.js
@@ -0,0 +1,15 @@
+/**
+ * @Author: xavier
+ * @Date:   2020-05-21T10:56:24-05:00
+ * @Last modified by:   xavier
+ * @Last modified time: 2020-05-22T08:08:07-05:00
+ */
+var info =[
+  {date:"white",name:"blanc"},
+  {date:"black",name:"negre"},
+  {date:"red",name:"vermell"},
+  {date:"orange",name:"taronja"},
+  {date:"green",name:"verd"},
+  {date:"blue",name:"blau"},
+  {date:"brown",name:"marró"}
+]
